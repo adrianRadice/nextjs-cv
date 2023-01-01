9a62a9a9a10d4cbaa89ca53b01ae256544e3309a
Adrian Radice <radiceadriann@gmail.com>
2023-01-01 20:57:02 -0300

    add seo data

---
+++ constants/portfolio-info.js
@@ -0,0 +1,17 @@
+export const seoData = {
+  title: "Adrian Radice",
+  description:
+    "A passionate Full Stack Web / Mobile Developer and Blockchain Developer.",
+  author: "Adrian Radice",
+  image: "https://avatars.githubusercontent.com/u/31006922",
+  url: "https://adrianradice.github.io/nextjs-cv",
+  keywords: [
+    "Radice",
+    "Radice Adrian",
+    "@adrianRadice",
+    "adrianRadice",
+    "Portfolio",
+    "Radice Portfolio ",
+    "Radice Adrian Portfolio",
+  ],
+};
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH,
+  },
   images: {
     loader: "akamai",
     path: "",
