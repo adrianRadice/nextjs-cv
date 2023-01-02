cb3549b2971ea556779de392a93580cc486973da
Adrian Radice <radiceadriann@gmail.com>
2023-01-02 09:22:33 -0300

    add greeting section

--- constants/portfolio-info.js
+++ constants/portfolio-info.js
@@ -15,3 +15,18 @@ export const seoData = {
     "Radice Adrian Portfolio",
   ],
 };
+
+export const greetings = {
+  name: "Adrian Radice",
+  title: "Hi all, I'm Adrian",
+  description:
+    "I'm passionate Full Stack web developer having an experience of web / mobile applications  with .net core, Go, React.js, React-Native, Next.js and serverless apps on Azure, AWS, Firebase and Blockchain development on Ethereum, Solidity, Web3.js.",
+  resumeLink: "",
+};
+
+export const socialLinks = {
+  url: "https://adrianradice.github.io/nextjs-cv",
+  linkedin: "https://www.linkedin.com/in/adrian-radice",
+  github: "https://github.com/adrianRadice",
+  instagram: "https://www.instagram.com/adrian.radice",
+};
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import dynamic from "next/dynamic";
 import SEO from "../components/SEO";
 import styles from "../styles/Home.module.css";
+const Navigation = dynamic(() => import("../components/Navigation"));
 
+const Greetings = dynamic(() => import("../containers/Greetings"));
 export default function Home() {
   return (
     <>
       <SEO />
+      <Navigation />
+      <Greetings />
       <main className={styles.main}></main>
     </>
   );
