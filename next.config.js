1765dea2bc0bf2c1ae0223d43b522ccc30f32be3
Adrian Radice <radiceadriann@gmail.com>
2023-01-02 13:57:48 -0300

    fix deps

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH,
+    NPM_CONFIG_LEGACY_PEER_DEPS: true,
   },
   images: {
     loader: "akamai",
